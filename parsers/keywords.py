bdf761283f77dffe58e760d6f2e6f814bd28499c
Matias Lindgren <matias.lindgren@gmail.com>
2017-04-29 18:02:30 +0800

    start writing parsers

---
+++ parsers/keywords.py
@@ -0,0 +1,15 @@
+"""
+Parsing data into and from internal keyword representation.
+"""
+import re
+
+BASEURL = "https://docs.python.org/3/library"
+
+def url_to_keyword(url):
+    if not url.startswith(BASEURL):
+        raise RuntimeError("Given url '{}' does not start with '{}'".format(url, BASEURL))
+    s = re.sub("({})|({})|({})".format(BASEURL, '/', '\.html'), "", url)
+    if "#" in s:
+        s = s.split("#")[-1]
+
+
