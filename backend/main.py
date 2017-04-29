763092b6c59135c4b178ff6c4fcf6e3752329174
Matias Lindgren <matias.lindgren@gmail.com>
2017-04-29 23:55:04 +0800

    show parsed results sorted

--- backend/main.py
+++ backend/main.py
@@ -6,13 +6,20 @@ flask_app = flask.Flask(__name__)
 
 def count_syntax(source_string):
     #TODO: extract and chop at least Call and Load nodes
-    c = collections.Counter()
+    #NOTE, this is not trivial, some sensible consistency must be invented before implementing this any further
+    counted_nodes = collections.Counter()
     class NodeVisitor(ast.NodeVisitor):
         def generic_visit(self, node):
-            c[node.__class__.__name__.split(".")[-1]] += 1
+            node_name = node.__class__.__name__
+            if node_name == "Call" and hasattr(node, 'func') and hasattr(node.func, 'id'):
+                counted_nodes["Function:" + node.func.id] += 1
+                # else:
+                    # raise RuntimeError("Found a Call node without 'func' attribute, Call node has fields {}".format(list(ast.iter_fields(node))))
+            else:
+                counted_nodes[node_name] += 1
             super().generic_visit(node)
     NodeVisitor().visit(ast.parse(source_string))
-    return c
+    return counted_nodes
 
 @flask_app.route("/")
 def index():
@@ -22,9 +29,11 @@ def index():
 def parse():
     render_context = dict()
     try:
-        given_spaghetti = flask.request.form['spaghetti']
-        # todo sort by keys
-        render_context["parsed_spaghetti"] = count_syntax(given_spaghetti).items()
+        # Parse spaghetti
+        raw_text = flask.request.form['spaghetti']
+        name_count = count_syntax(raw_text)
+        sorted_name_counts = ((key, name_count[key]) for key in sorted(name_count.keys()))
+        render_context["parsed_spaghetti"] = sorted_name_counts
     except SyntaxError as syntax_error:
         render_context["errors"] = str(syntax_error)
     return flask.render_template('parsed.html', **render_context)
