2ea17529c5e08b5744db014238e2b5afcae08124
Matias Lindgren <matias.lindgren@gmail.com>
2017-05-01 00:06:57 +0800

    rewrite spider to scrape code snippets on tutorial pages

--- pydocs_scraper/pydocs_scraper/settings.py
+++ pydocs_scraper/pydocs_scraper/settings.py
@@ -65,7 +65,8 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'pydocs_scraper.spiders.doc_spider.WriterPipeline': 100
+    'pydocs_scraper.pipelines.DuplicatesPipeline': 100,
+    # 'pydocs_scraper.spiders.doc_spider.WriterPipeline': 200
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
--- pydocs_scraper/pydocs_scraper/spiders/doc_spider.py
+++ pydocs_scraper/pydocs_scraper/spiders/doc_spider.py
@@ -1,79 +1,81 @@
 """
-Scrapy spider for crawling all links on the python docs library page
-TODO:
- - scrap LINK_CACHE and use some external service such as redis
+Scrapy spiders for crawling the Python docs.
 """
 import scrapy
-import collections
 import re
 
 
 class PATTERN:
-    TOCTREE_LINKS = "//ul/li[contains(@class, 'toctree')]/a"
-    PAGE_SECTIONS = "//div[contains(@class, 'section')]/dl"
-    HREF = ".//@href"
-    SECTION_LINKS = ".//dt/a[contains(@class, 'headerlink')]/@href"
-    INTERNAL_LINKS = ".//dd//a[contains(@class, 'internal reference') or contains(@class, 'reference internal')]/@href"
-    TEXT = ".//text()"
-    SHELL_STRINGS = (
+    """
+    XPath selectors and other string constants.
+    """
+    _SECTION_DIV         = "div[contains(@class, 'section')]"
+    ALL_PAGE_SECTIONS    = ".//" + _SECTION_DIV
+    CHILD_PAGE_SECTIONS  = "./" + _SECTION_DIV
+    LIST_PAGE_SECTIONS   = "./{}/dl".format(_SECTION_DIV)
+    TOCTREE_TOP_LINKS    = "//ul/li[contains(@class, 'toctree-l1')]/a"
+    SECTION_LINKS        = ".//dt/a[contains(@class, 'headerlink')]/@href"
+    HEADER_SECTION_LINKS = "|".join("(.//h{}/a[contains(@class, 'headerlink')]/@href)".format(i) for i in range(1, 5))
+    INTERNAL_LINKS       = ".//dd//a[contains(@class, 'internal reference') or contains(@class, 'reference internal')]/@href"
+    CHILD_CODE_SNIPPETS  = "./div[contains(@class, 'highlight-python3')]"
+    HREF                 = ".//@href"
+    ALL_TEXT             = ".//text()"
+    SHELL_STRINGS        = (
         ">>>",
         "...",
     )
-    # REFERENCE
-    # div class=section id='name'
-    #   p p p
-    #
 
-def parse_highlighted_code(selector):
-    def contains_shell_characters(string):
+
+def parse_highlighted_code(snippet_selector):
+    """
+    Return the html highlighted code selected by the scrapy.Selector given as parameter as a plain string without shell characters.
+    """
+    def no_shell_characters_in(string):
         shell_pattern = re.compile("|".join("^" + re.escape(p) for p in PATTERN.SHELL_STRINGS))
-        return re.search(shell_pattern, string) is not None
-    code_text = selector.xpath(PATTERN.TEXT).extract()
-    return ''.join(s for s in code_text if not contains_shell_characters(s))
+        return re.search(shell_pattern, string) is None
+    unhighlighted_code_pieces = snippet_selector.xpath(PATTERN.ALL_TEXT).extract()
+    return ''.join(filter(no_shell_characters_in, unhighlighted_code_pieces))
 
 
-class DocSpider(scrapy.Spider):
-    name = "docs"
+class TutorialSpider(scrapy.Spider):
+    name = "tutorial"
     start_urls = (
-        # 'http://docs.python.org/3/library',
-        # 'http://docs.python.org/3/reference',
-        'localhost:8000/reference',
+        # 'http://docs.python.org/3/tutorial',
+        'http://localhost:8000/tutorial',
     )
-    LINK_CACHE = collections.defaultdict(set)
 
     def parse(self, response):
         self.logger.debug("Parsing response {}".format(response))
-        for page in response.xpath(PATTERN.TOCTREE_LINKS):
+        for page in response.xpath(PATTERN.TOCTREE_TOP_LINKS):
             next_url = response.urljoin(page.xpath(PATTERN.HREF).extract_first())
-            self.logger.debug("Requesting {}".format(next_url))
             yield scrapy.Request(next_url, self.parse_page)
 
-    def parse_page(self, response):
-        self.logger.debug("Parsing page {}".format(response))
-        for section in response.xpath(PATTERN.PAGE_SECTIONS):
-            # print("parsing section {}".format(section))
-            section_name = section.xpath(PATTERN.HREF).extract_first()
-            # print("section_name {}".format(section_name), end=';')
-            section_url = section.xpath(PATTERN.SECTION_LINKS).extract_first()
-            internal_links = section.xpath(PATTERN.INTERNAL_LINKS).extract()
-            self.store_edges(
-                response.urljoin(section_url),
-                map(response.urljoin, internal_links)
-            )
+    def parse_page(self, page):
+        """
+        Helper method for starting the parse of all sections on 'page' from the root section.
+        """
+        self.logger.debug("Parsing page {}".format(page))
+        sections = page.xpath(PATTERN.ALL_PAGE_SECTIONS)
+        if not sections:
+            self.logger.warning("Page has no parsable sections")
+            return None
+        yield from self.parse_section(sections[0])
 
-    def store_edges(self, from_url, to_urls):
-        self.__class__.LINK_CACHE[from_url]
-        for url in to_urls:
-            self.__class__.LINK_CACHE[from_url].add(url)
-
-
-class WriterPipeline:
-    def close_spider(self, spider):
-        spider.logger.debug("Closing spider {}".format(spider))
-        with open("out", "w") as f:
-            spider.logger.debug("Writing output from cache, which has {} items".format(len(DocSpider.LINK_CACHE)))
-            for from_url, to_urls in DocSpider.LINK_CACHE.items():
-                f.write(from_url + ";" + ",".join(to_urls) + "\n")
-
-
+    def parse_section(self, section):
+        """
+        Recursively yield dicts of url string and code snippets list within 'section' and all its nested sections.
+        The data within a nested section is not included into the data of its parent, unless it is explicitly duplicate in the html.
+        """
+        def get_section_url(s):
+            return s.xpath(PATTERN.HEADER_SECTION_LINKS).extract_first()
+        section_url = get_section_url(section)
+        self.logger.debug("Parsing section {}".format(section_url))
+        for subsection in section.xpath(PATTERN.CHILD_PAGE_SECTIONS):
+            yield from self.parse_section(subsection)
+        snipped_selector = section.xpath(PATTERN.CHILD_CODE_SNIPPETS)
+        code_snippets = list(map(parse_highlighted_code, snipped_selector))
+        yield {
+            "url": section_url,
+            "code_snippets": code_snippets
+        }
 
