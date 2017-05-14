334379f51812232c7db6411c384898f4069ec9e4
Matias Lindgren <matias.lindgren@gmail.com>
2017-05-14 11:28:17 +0800

    move result parsing to indexer

--- src/backend/indexer.py
+++ src/backend/indexer.py
@@ -1,12 +1,15 @@
 import os.path
 import ast
 import ast_parser
+import pygments
+from pygments.lexers import Python3Lexer
+from pygments.formatters import HtmlFormatter
 from whoosh.index import exists_in, create_in, open_dir
 from whoosh.fields import Schema, TEXT, ID
 from whoosh.query import Term
 
 
-def create_new_index(path, name, **tokenizer_options):
+def create_new_index(path, name, tokenizer_options):
     schema = Schema(
         title=TEXT(stored=True),
         url=ID(stored=True, unique=True),
@@ -18,13 +21,18 @@ def create_new_index(path, name, **tokenizer_options):
     return create_in(path, schema, indexname=name)
 
 
+def all_linenumbers(root):
+    return [node.lineno for node in ast.walk(root) if hasattr(node, "lineno")]
+
+
+
 #TODO incremental indexing and housekeeping
 class Index:
-    def __init__(self, index_path, name, **tokenizer_options):
+    def __init__(self, index_path, name, tokenizer_options):
         if exists_in(index_path, name):
             self.index = open_dir(index_path, name)
         else:
-            self.index = create_new_index(index_path, name, **tokenizer_options)
+            self.index = create_new_index(index_path, name, tokenizer_options)
         self.name = name
         self.tokenizer_options = tokenizer_options
 
@@ -52,4 +60,35 @@ class Index:
         with self.index.searcher() as searcher:
             for result in searcher.search(self.parse_query(code_query), terms=True):
                 yield result
+
+    def get_similar_snippets(self, code):
+        similar = []
+        for hit in self.get_documents(code):
+            data = {'title': hit['title'], 'url': hit['url'], 'matched': hit.matched_terms()}
+            matched_tokens = set(pair[1] for pair in hit.matched_terms() if pair[0] == 'content')
+            data['content'] = self.highlight_matches(hit['content'], matched_tokens)
+            similar.append(data)
+        return similar
+
+
+    # TODO: implement a custom lexer to highlight matching tokens instead of the whole line containing a matching token
+    def highlight_matches(self, code, matched_tokens):
+        line_numbers = list()
+        for node in ast.walk(ast.parse(code)):
+            dumps = ast_parser.dump(node, **self.tokenizer_options)
+            node_dump = next(dumps, '').encode()
+            if node_dump in matched_tokens:
+                line_numbers.extend(all_linenumbers(node))
+        return self.html_highlight(code, line_numbers)
+
+
+    def html_highlight(self, code, line_numbers):
+        format_options = {
+            "hl_lines": line_numbers,
+        }
+        return pygments.highlight(
+            code,
+            Python3Lexer(),
+            HtmlFormatter(**format_options)
+        )
 
--- src/backend/main.py
+++ src/backend/main.py
@@ -19,48 +19,11 @@ def parse():
     try:
         # Parse spaghetti
         raw_text = flask.request.form['spaghetti']
-        similar_snippets = get_similar_snippets(raw_text)
-        print("{} similar snippets".format(len(similar_snippets)))
+        similar_snippets = index.get_similar_snippets(raw_text)
         render_context["similar"] = similar_snippets
     except SyntaxError as syntax_error:
         render_context["errors"] = str(syntax_error)
     return flask.render_template('parsed.html', **render_context)
-
-
-def get_similar_snippets(code):
-    similar = []
-    for hit in index.get_documents(code):
-        data = {'title': hit['title'], 'url': hit['url'], 'matched': hit.matched_terms()}
-        matched_tokens = set(pair[1] for pair in hit.matched_terms() if pair[0] == 'content')
-        data['content'] = highlight_matches(hit['content'], matched_tokens)
-        similar.append(data)
-    return similar
-
-
-def all_linenumbers(root):
-    return [node.lineno for node in ast.walk(root) if hasattr(node, "lineno")]
-
-
-# TODO: implement a custom lexer to highlight matching tokens instead of the whole line containing a matching token
-def highlight_matches(code, matched_tokens):
-    line_numbers = list()
-    for node in ast.walk(ast.parse(code)):
-        dumps = ast_parser.dump(node, tokenize_leaves=False)
-        node_dump = next(dumps, '').encode()
-        if node_dump in matched_tokens:
-            line_numbers.extend(all_linenumbers(node))
-    return html_highlight(code, line_numbers)
-
-def html_highlight(code, line_numbers):
-    format_options = {
-        "hl_lines": line_numbers,
-    }
-    return pygments.highlight(
-        code,
-        Python3Lexer(),
-        HtmlFormatter(**format_options)
-    )
-
 
 
 if __name__ == "__main__":
