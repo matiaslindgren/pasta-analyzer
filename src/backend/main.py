e360cc799839784c6e8da23c438247d5189d1d15
Matias Lindgren <matias.lindgren@gmail.com>
2017-06-02 19:48:50 +0800

    update main

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,6 @@
 import flask
 import init_apps
-import argparse
+import json
 
 
 flask_app = init_apps.make_flask(__name__)
@@ -9,24 +9,26 @@ index = init_apps.load_index()
 
 @flask_app.route("/")
 def root():
-    return flask.render_template('index.html')
+    return flask.render_template("index.html", is_index_page=True)
 
 
 @flask_app.route("/parse")
 def parse():
     render_context = dict()
     try:
-        raw_text = flask.request.args.get('spaghetti')
+        raw_text = flask.request.args.get("spaghetti").lstrip()
+        render_context["previous_input"] = raw_text
         print(raw_text)
         similar_snippets = list(index.get_similar_snippets(raw_text))
         render_context["similar"] = similar_snippets
+        render_context["has_results"] = len(similar_snippets) > 0
     except SyntaxError as syntax_error:
         render_context["errors"] = str(syntax_error)
     if flask.request.args.get("hasJavascript"):
         print("has javascript")
         return json.dumps(render_context)
     print("no javascript")
-    return flask.render_template('parsed.html', **render_context)
+    return flask.render_template("index.html", **render_context)
 
 
 if __name__ == "__main__":
