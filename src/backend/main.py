ac40a90684bf3639818b094f4d30022627adc38d
Matias Lindgren <matias.lindgren@gmail.com>
2017-06-02 15:47:28 +0800

    no need to accept data with the post method

--- src/backend/main.py
+++ src/backend/main.py
@@ -12,15 +12,20 @@ def root():
     return flask.render_template('index.html')
 
 
-@flask_app.route("/parse", methods=("POST", ))
+@flask_app.route("/parse")
 def parse():
     render_context = dict()
     try:
-        raw_text = flask.request.form['spaghetti']
+        raw_text = flask.request.args.get('spaghetti')
+        print(raw_text)
         similar_snippets = list(index.get_similar_snippets(raw_text))
         render_context["similar"] = similar_snippets
     except SyntaxError as syntax_error:
         render_context["errors"] = str(syntax_error)
+    if flask.request.args.get("hasJavascript"):
+        print("has javascript")
+        return json.dumps(render_context)
+    print("no javascript")
     return flask.render_template('parsed.html', **render_context)
 
 
