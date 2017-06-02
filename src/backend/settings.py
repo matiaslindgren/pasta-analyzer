415887e87ae8138dc7f74fcb8d0eebd967b0b31b
Matias Lindgren <matias.lindgren@gmail.com>
2017-06-02 15:47:50 +0800

    match code structure instead of names

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -1,5 +1,12 @@
 INDEX_DIRNAME = "index"
 INDEX_NAME = "simple_index"
+
 TOKENIZER_OPTIONS = {
-    'min_depth': 2
+    # Ignore variable, function, class and argument names
+    'drop_field_names': {"id", "arg", "name"},
+    # Do not compare the root nodes of two syntax trees
+    'drop_node_names': {"Module"},
+    # Minimum depth of a matching syntax tree
+    'min_depth': 4
 }
+
